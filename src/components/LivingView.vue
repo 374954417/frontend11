e7fa55bfc2640f79482ed27401bbe56ae2f33554
374954417 <374954417@qq.com>
2023-05-10 22:39:52 +0800

    index first commit

---
+++ src/components/LivingView.vue
@@ -0,0 +1,12 @@
+<template>
+
+
+  <div>
+    <el-row style="margin-left: 30vw"><el-text tag="b" style="font-size: 60px;color: white">热门住宿</el-text></el-row>
+  </div>
+
+</template>
+
+<script>
+
+</script>
